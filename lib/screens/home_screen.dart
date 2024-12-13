94db2732d82131d10e65772dd9e3d40f502c459c
beingraigistani <mtayabmahar186@gmail.com>
2024-12-13 22:47:55 +0500

    search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,27 +9,70 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text(
           'Pakistani WhatsApp',
-          style: TextStyle(color: Colors.green,fontSize: 15),
+          style: TextStyle(color: Colors.green, fontSize: 15),
         ),
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.camera_front),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.camera_front),
           ),
-          IconButton(onPressed: (){}, icon: const Icon(Icons.more_vert),)
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          )
         ],
       ),
       body: const SingleChildScrollView(
         child: Column(
           children: [
-
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(30),
+                      ),
+                    ),
+                    fillColor: Color.fromARGB(240, 246, 245, 243),
+                    filled: true,
+                    hintText: 'Ask Meta AI or Search'),
+              ),
+            ),
           ],
         ),
       ),
       bottomNavigationBar: BottomAppBar(
         child: Row(
           children: [
-            IconButton(onPressed: (){}, icon: const Icon(Icons.chat)),
-            ElevatedButton(onPressed: (){}, child: const Icon(Icons.call),)
-
+            Column(
+              children: [
+                ElevatedButton(onPressed: () {}, child: Icon(Icons.chat)),
+                Text('Chats')
+              ],
+            ),
+            Spacer(),
+            Column(
+              children: [
+                ElevatedButton(onPressed: () {}, child: Icon(Icons.update)),
+                Text('Updates')
+              ],
+            ),
+            Spacer(),
+            Column(
+              children: [
+                ElevatedButton(onPressed: () {}, child: Icon(Icons.group)),
+                Text('Communities')
+              ],
+            ),
+            Spacer(),
+            Column(
+              children: [
+                ElevatedButton(onPressed: () {}, child: Icon(Icons.call)),
+                Text('Calls')
+              ],
+            ),
           ],
         ),
       ),
